c56b71ec59cbdc3b92dfe060f10cc38da3495bd9
rohitsraj12 <rohitsraj12@gmail.com>
2020-03-30 06:51:20 +0530

    foundation lessons of jQuery

---
+++ jQuery/foundation/selectors/day-1/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // * will select all element from DOM
+  $("*").css("textAlign", "center");
+
+  // This would select all the div
+  $("p").css("background-color", "yellow");
+
+  // this would select all class names of a
+  $(".group").css({
+    fontSize: "40px"
+  });
+
+  // this would select perticuler perticular class name
+  $(".foo").css("width", "50%");
+
+  // this would select perticuler ID
+  $("#box-3").css({
+    width: "80%",
+    color: "red"
+  });
+
+  // multi class selection (can select id as well as elements)
+  $(".foo, .delta").css("margin", "auto");
+});
