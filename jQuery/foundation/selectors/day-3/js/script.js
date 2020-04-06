0dbdcfa49967303bd6ba5201bb263f5ed8c56b49
rohitsraj12 <rohitsraj12@gmail.com>
2020-04-06 17:19:39 +0530

    Create script.js

---
+++ jQuery/foundation/selectors/day-3/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // selecting first child of ID name called first_child
+  $(".foo li:first-child").css("color", "red");
+
+  // selecting last child of ID name called first_child
+  $(".bar li:last-child").css("backgroundColor", "blue");
+});
