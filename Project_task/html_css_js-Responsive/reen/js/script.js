7cc71b267f306b45967c545859560adfd592e694
rohitsraj12 <rohitsraj12@gmail.com>
2021-06-09 21:25:25 +0530

    Update script.js

--- Project_task/html_css_js-Responsive/reen/js/script.js
+++ Project_task/html_css_js-Responsive/reen/js/script.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
-    $("#toggle-nav").click(function(){
-        $("#header-nav").slideToggle(750);
-    });
+$(document).ready(function () {
+  $("#toggle-nav").click(function () {
+    $("#header-nav").slideToggle(750);
+  });
+
+  $(".owl-carousel").owlCarousel({
+    stagePadding: 0,
+    loop: false,
+    margin: 20,
+    nav: false,
+    dotClass: "owl-dot",
+    dotsClass: "owl-dots",
+    navText: [
+      "<i class='fa fa-long-arrow-left owl__left-arrow'></i>",
+      "<i class='fa fa-long-arrow-right owl__right-arrow'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 2,
+      },
+      582: {
+        items: 3,
+      },
+      960: {
+        items: 4,
+      },
+    },
+  });
 });
